c700b6335f383f506090b63108626e646d6e4e57
mishrpranav007 <59594377+mishrpranav007@users.noreply.github.com>
2020-02-13 19:05:30 +0530

    Updates

---
+++ precache-manifest.0aeac591d7059ef14813b20dc5e88e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d52e285caa5ae0cf01f6ef0dad877fb",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "b746e9a5ba6a8d1c51aa",
+    "url": "/recipeapp/static/css/main.a9265c4c.chunk.css"
+  },
+  {
+    "revision": "d70f9a24225c480ac06b",
+    "url": "/recipeapp/static/js/2.7d4dec59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipeapp/static/js/2.7d4dec59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b746e9a5ba6a8d1c51aa",
+    "url": "/recipeapp/static/js/main.26cbbe28.chunk.js"
+  },
+  {
+    "revision": "2c5fe7fbb88bf592b6db",
+    "url": "/recipeapp/static/js/runtime-main.d175cd38.js"
+  }
+]);
